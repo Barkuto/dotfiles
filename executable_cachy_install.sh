1644c7ac7ecefd2835d90bcc8ea5b449403f452d
Iggie <pohatu678@msn.com>
2026-08-27 18:26:14 -0400

    Update cachy_install.sh

--- executable_cachy_install.sh
+++ executable_cachy_install.sh
@@ -16,12 +16,11 @@
 #session    optional     pam_gnome_keyring.so auto_start
 
 curl -fsSL https://install.danklinux.com | sh
-paru -S --noconfirm firefox yazi cachy-update xdg-terminal-exec visual-studio-code-bin packet rustdesk-bin onlyoffice-bin hyprpolkitagent xdg-desktop-portal-hyprland gnome-keyring libsecret
+paru -S --noconfirm firefox yazi cachy-update xdg-terminal-exec packet rustdesk-bin onlyoffice-bin hyprpolkitagent xdg-desktop-portal-hyprland gnome-keyring libsecret
 paru -S --noconfirm cups-pk-helper ttf-fira-code inter-font
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 paru -S --noconfirm chezmoi github-cli
+paru -S --noconfirm qt6ct-kde gnome-themes-extra
 
 #echo kitty.desktop >>  ~/.config/xdg-terminals.list
-arch-update --tray --enable
-systemctl --user enable --now arch-update-tray.service
 sudo ip rule add to 192.168.50.0/24 priority 5000 table main
